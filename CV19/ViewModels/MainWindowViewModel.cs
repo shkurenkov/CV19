7118b4f024f863c767d619e446f430b73eac317d
Shkurenkov <shkurenkov.alec@outlook.com>
2021-05-04 10:51:11 +0300

    Тестовая разметка окна

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return false;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
